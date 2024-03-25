4467dac364ba30e2cb6b0df6e73b5cf5bee98180
tatihdz <119941402+tatihdz@users.noreply.github.com>
2024-03-25 07:54:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    botões[i].onclickssss
-    console.log();
+    botoes[i].onclick = function(){
+    
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+    botoes[i].classList.add("ativo")
+    }
+
 }
