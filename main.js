58b645bdf0d882a2f85f17ab12e979acc7e44fa7
tatihdz <119941402+tatihdz@users.noreply.github.com>
2024-04-18 08:48:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,15 +15,12 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2014-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
 const tempos = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,]
-
-contadores[0].textContent = calculaTempo(tempos[0]);
-
 
 
 function calculaTempo(tempoObjetivo){
@@ -33,13 +30,30 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
-
+    
     segundos %= 60; 
     minutos %= 60;
     horas %= 24;
-
-
+if(tempoFinal > 0) {
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-    
-    
+
+} else{
+    return "Prazo Finalizado";
 }
+
+}
+    
+function atualizaCronometro(){
+    for(let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+
+}
+    
+ //comecaCronometro();
+    
